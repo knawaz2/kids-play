5e329936f43569570eb4e391fb422442cfeb4d08
Kashif <kashif.randhawa@gmail.com>
2017-05-16 12:33:36 +0400

    DB Path updated

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-databasepath: 'mongodb://localhost:27017/kidsplay',
-
+databasepath: 'mongodb://kidsplaydb:bcss01a037@ds143211.mlab.com:43211/kidsplay',
 secret: 'anysecretstring'
 
 }
+//databasepath: 'mongodb://localhost:27017/kidsplay',
